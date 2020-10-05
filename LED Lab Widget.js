ecc55960ad79f97613434ea5a90836eef90e42f7
Ryan <rkersten@students.greenhillsschool.org>
2020-10-05 17:23:57 -0400

    Initial Commit
    Doesn't have uncertainty calculation support

---
+++ LED Lab Widget.js
@@ -0,0 +1,22 @@
+function calcValues(){
+    var A = parseFloat(document.getElementById("I1").value);
+    var B = parseFloat(document.getElementById("I2").value);
+    var C = parseFloat(document.getElementById("I3").value);
+    var RMSE= parseFloat(document.getElementById("I4").value);
+
+
+
+    x = clac(A,B,C);
+    document.getElementById("O1").innerHTML = x + " V";
+
+
+
+}
+
+function clac(a,b,c){
+    let y = .00001 - b;
+    y = y / a;
+    y = Math.log(y);
+    y = y/c;
+    return -y;
+}
